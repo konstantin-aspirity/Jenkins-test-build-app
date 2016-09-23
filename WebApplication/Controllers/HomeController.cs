53a53282f9b55849cd37c9aab90d653f46034100
Konstantin Yushkin <ky@aspirity.com>
2016-09-23 13:42:21 +0700

    -

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication.Properties;
 
 namespace WebApplication.Controllers
 {
@@ -10,6 +11,7 @@ namespace WebApplication.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Env = Settings.Default.Env;
             return View();
         }
 
